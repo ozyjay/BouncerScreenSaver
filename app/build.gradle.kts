d5bf9e8a2de4c98509e5f035d563c70f8e72052a
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2026-07-27 20:42:59 +1000

    Configure NDK debug symbol level for release builds
    - Add `debugSymbolLevel = "SYMBOL_TABLE"` to the `release` build type in `app/build.gradle.kts` to enable native crash symbolication in the Play Console.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,9 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
