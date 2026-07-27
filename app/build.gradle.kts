b1b35e5a287d7370c9e903c58af3ccf03dbecd63
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2026-07-27 20:54:43 +1000

    Increment version code to 3 and version name to 1.2
    - Update `versionCode` to `3` and `versionName` to `"1.2"` in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.crunchycodes.bouncer.live.wallpaper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
